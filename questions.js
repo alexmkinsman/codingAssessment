49e3fdf966b380ee3d7c3ae7f2ba47268b9eff30
alexmkinsman <akinsman825@gmail.com>
2022-09-07 11:42:28 -0700

    added js files for storing the high scores and q's

---
+++ questions.js
@@ -0,0 +1,46 @@
+let questions = [
+  {
+      Number: 1,
+      question: "What is an Array?",
+      answer: "Collection of items stored in a variable name",
+      options: [
+          "The name assigned to a variable",
+          "A method used to define objects",
+          "Collection of items stored in a variable name",
+          "A complex function"
+      ]
+  },
+  {
+    Number: 2,
+    question: "Conditionals use ___ statements.",
+    answer: "if",
+    options: [
+        "if",
+        "or",
+        "and",
+        "then"
+    ]
+  },
+  {
+    Number: 3,
+    question: "Which of the following cannot be used as a variable name?",
+    answer: "1day",
+    options: [
+        "oneDay",
+        "1day",
+        "one_day",
+        "All of the above can be used"
+    ]
+  },
+  {
+    Number: 4,
+    question: "_______ is the process of finding errors and fixing them within a program.",
+    answer: "Debugging",
+    options: [
+        "Compiling",
+        "Refactoring",
+        "Scanning",
+        "Debugging"
+    ]
+  },
+];
